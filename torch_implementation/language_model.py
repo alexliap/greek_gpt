e12f6e4b458258a69e4d285f36cb55ac37b48c7d
alexliap <alexandrosliapates@gmail.com>
2024-07-04 21:47:01 +0300

    stuff

--- torch_implementation/language_model.py
+++ torch_implementation/language_model.py
@@ -55,26 +55,14 @@ class LanguageModel(L.LightningModule):
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         x, y = batch
+
         logits = self.forward(x)
         loss = F.cross_entropy(logits, y.view(-1), reduction='mean')
+
+        self.log("train_ce_loss", loss, prog_bar=True)
+
         return loss
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(params=self.parameters(), lr=self.lr)
         return optimizer
-
-# B, T = 128, 256
-# model = LanguageModel(
-#     vocab_size=5000,
-#     n_embed=256,
-#     context_len=T,
-#     n_blocks=8,
-#     n_heads=8,
-#     n_experts=4,
-#     top_k=2,
-#     lr=1e-4,
-# ).bfloat16()
-# total_params = sum(p.numel() for p in model.parameters())
-# print(round(total_params/1e6, 3))
-# x = torch.randint(0, 5000, (B, T))
-# print(model(x).shape)
