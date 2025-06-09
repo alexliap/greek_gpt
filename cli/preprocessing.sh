982492a06e8ec263c554fa763dbf4519dd284509
root <alexandrosliapates@gmail.com>
2025-06-09 12:38:04 +0000

    change a bit the messages

--- cli/preprocessing.sh
+++ cli/preprocessing.sh
@@ -10,8 +10,8 @@ echo "Saved Greek version of Wikipedia locally in a text file."
 
 python ./cli/preprocessing/train_tokenizer.py --vocab-size 5000
 
-echo "Trained 4 new GPT2 tokenizers from scratch with sizes: 1000 | 2000 | 5000 | 10000."
+echo "Trained new GPT2 tokenizer from scratch."
 
 python ./cli/preprocessing/tokenize_data.py --vocab-size 5000
 
-echo "Tokenized the dataset with all tokenizers, resulting in 4 versions."
+echo "Tokenized the dataset with the selected tokenizer."
